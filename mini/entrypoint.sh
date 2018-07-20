1b15b72e342ee8e4544715fd5f6ae6bde96ded4e
Simon Mandlik <simon.mandlik@gmail.com>
2018-07-20 12:51:32 +0200

    ...

--- mini/entrypoint.sh
+++ mini/entrypoint.sh
@@ -2,5 +2,4 @@
 
 s3fs $S3_BUCKET $S3 -o use_cache=/tmp -o iam_role=NN-EC2-Worker -o umask=002 -o use_sse=kmsid:'KMSID'
 julia --depwarn=no -e "Pkg.activate(\"/root\"); Pkg.instantiate()"
-
 exec "$@"
