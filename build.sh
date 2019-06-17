3e484c96eed29500e126da9e2d0e2ac58e872074
Simon Mandlik <simon.mandlik@gmail.com>
2019-06-17 16:56:44 +0200

    ...

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# e.g. sudo ./build.sh $M1 2018-09-26_all_4_1
-if [ "$#" -ne 2 ]; then
-    echo "Please supply result folder as an argument"
+# e.g. sudo ./build.sh $M1 2018-09-26_all_4_1 latest
+if [ "$#" -ne 3 ]; then
+    echo "Please supply machine from which to pull model, the model name, and the container tag as an arguments"
     exit 1
 fi
 
@@ -11,14 +11,14 @@ cp -r ~/src/ ./mini/data/
 scp $1:/home/ubuntu/s3/data/results/$2/training/m.bson ./mini/data/m.bson || exit 1
 scp $1:/home/ubuntu/s3/data/results/$2/training/training_norm_consts.jld2 ./mini/data/training_norm_consts.jld2 || exit 1
 
-# build julia-custom and upload
+# # build julia-custom and upload
 cd julia-custom
 sudo docker build ./ --tag simonmandlik/julia-custom:latest
 sudo docker push simonmandlik/julia-custom:latest
-# build mini and upload
+# # build mini and upload
 cd ../mini
-sudo docker build ./ --tag simonmandlik/mini:baseline
-sudo docker push simonmandlik/mini:baseline
+sudo docker build ./ --tag simonmandlik/mini:$3
+sudo docker push simonmandlik/mini:$3
 
-# sudo docker run --rm --privileged simonmandlik/mini:latest hobbiton-eu-west-1-nn docker_test
-# or sudo docker run --rm -it --privileged --entrypoint=/bin/bash simonmandlik/mini:latest hobbiton-eu-west-1-nn docker_test
+# sudo docker run --rm --privileged simonmandlik/mini:baseline hobbiton-eu-west-1-nn docker_test
+# or sudo docker run --rm -it --privileged --entrypoint=/bin/bash simonmandlik/mini:baseline hobbiton-eu-west-1-nn docker_test
